ef66d3f7ba3427314cc71a356bafd075c5eb7907
flosant <ugflosant@gmail.com>
2025-05-30 20:04:11 -0600

    se modifico el footer por datos

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,5 +1,5 @@
 export const AppConfig = {
-  site_name: 'Under construction',
+  site_name: 'Tepaxapa',
   title: 'Tepaxapa',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS, this blog is under construction page only',
   locale: 'en',
