25fe5257d4b1939b5b1860b2b2d822dc97a75098
flosant <ugflosant@gmail.com>
2025-06-09 14:37:17 -0600

    merge local

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -5,11 +5,7 @@ const Sponsors = () => (
     title="Nuestra historia, nuestro orgullo"
     description="Desde nuestras raices indígenas hasta las celebraciones actuales, Tepaxapa tiene una historia viva que merece ser contada"
   >
-<<<<<<< HEAD
     ´.´
-=======
-    {null}
->>>>>>> 8e703826214ed8f36df9093c5e6f63d6482b025f
   </Section>
 );
 
