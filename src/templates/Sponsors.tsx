a279e6f5688ca467798423d0406a8f6fa709c119
Gustavo Flores <60217486+F10S4NT@users.noreply.github.com>
2025-06-09 13:36:27 -0600

    Update Sponsors.tsx
    se puso null en sponsor

--- src/templates/Sponsors.tsx
+++ src/templates/Sponsors.tsx
@@ -3,9 +3,9 @@ import { Section } from '@/layout/Section';
 const Sponsors = () => (
   <Section
     title="Nuestra historia, nuestro orgullo"
-    description="Desde nuetras raices indígenas hasta las celebraciones actuales, Tepaxapa tiene una historia viva que merece ser contada"
+    description="Desde nuestras raices indígenas hasta las celebraciones actuales, Tepaxapa tiene una historia viva que merece ser contada"
   >
-   
+    {null}
   </Section>
 );
 
